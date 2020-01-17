6e9d5a85c91f66dabc933664581076c1eae17478
FernandoVeliz <39470687+FernandoVeliz@users.noreply.github.com>
2020-01-16 23:00:22 -0600

    Traducción y plantilla de prueba

--- www/index.php
+++ www/index.php
@@ -7,14 +7,14 @@ site_header("Home");
 <br />
 </noscript>
 
-Welcome to the Bell System Web <abbr title="User Interface">UI</abbr>. If this is the first time you've ever been to this site, go through the pages in the order they are listed in the menu to setup the bell system.
+Welcome to the Bell System Web <abbr title="User Interface">UI</abbr>. Si esta es la primera vez que visita el sitio, vaya en orden por cada una de las categorías para configurar el timbre.
 <br /><br />
 <ul>
-<li><b>Horarios</b> &mdash; Crea horarios que hacen sonar la campana en una hora específica</li>
+<li><b>Horarios</b> &mdash; Crea horarios que hacen sonar el timbre en una hora específica</li>
 <li><b>Calendario</b>  &mdash; Configura cuándo se van a utilizar estos horarios</li>
-<li><b>Opciones</b>  &mdash; Configura cuándo inicia/termina la escuela para activar la campana</li>
-<li><b>Respaldo</b>    &mdash; Respalda el archivo de configuración de la campana</li>
+<li><b>Opciones</b>  &mdash; Configura cuándo inicia/termina la escuela para activar el timbre</li>
+<li><b>Respaldo</b>    &mdash; Respalda el archivo de configuración del timbre</li>
 </ul>
 
-<p>If you have any questions, you can consult the <a href="http://floft.net/wiki/Bells/Docs.html" target="_blank">documentation</a>.</p>
+
 <?php site_footer(); ?>
